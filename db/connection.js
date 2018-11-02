e18452c7dc80f481308c6bc2e70c6ac542dfb1cf
Shivank Rao <shivankrao@Shivanks-MacBook-Pro.local>
2018-11-02 17:05:10 -0400

    changed connection file

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,21 @@
-const mongoose = require('mongoose');
-require('dotenv').config()
+require('dotenv').config();
+const mongoose = require('mongoose')
 
-//Configure mongoose's promise to global promise
-mongoose.promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI); 
-
-mongoose.connect('mongodb://localhost/itinerator2')
-
-mongoose.connection.once('open', () => {
-    console.log(`Mongoose has connected to MongoDB`)
-})
-mongoose.connection.on('error', (error) => {
-    console.error(`MongoDB connection error!!! ${error}`)
-    process.exit(-1)
-})
+// Connect to database
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+    mongoose.connect('mongodb://localhost/Self-Symmetry');
+}
+mongoose.connection.on('error', function (err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+}
+);
+mongoose.connection.once('open', function () {
+    console.log("Mongoose has connected to MongoDB!");
+});
 
 module.exports = mongoose
